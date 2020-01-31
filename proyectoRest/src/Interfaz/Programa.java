9bfab0de29469f4361e0b852ccad297c6f447122
Steven Gaibor <degacoli@espol.edu.ec>
2020-01-31 00:26:14 -0500

    Agregadas nuevas imágenes para la pantalla de Inicio de Sesión. Reedición de la intfaz gráfica de la panatlla de Inicio de Sesión de modo que resulte visualmente más atractiva. Reestructuración de los nodos de la panatlla de Inicio de Sesión.

--- proyectoRest/src/Interfaz/Programa.java
+++ proyectoRest/src/Interfaz/Programa.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Interfaz;
 
 import Actores.Administrador;
@@ -22,12 +17,15 @@ import javafx.stage.Stage;
  *
  * @author Eddy Santibañez J
  */
-public class Programa extends Application {
+public class Programa extends Application
+{
     static  ArrayList<Producto> productos;
     static Restaurante restaurante;
     static Scene sc;
-   @Override
-    public void init(){
+    
+    @Override
+    public void init ( )
+    {
         //lo de los meseros usuarios tambien es en un archivo
         restaurante= new Restaurante(); 
         Mesero m1= new Mesero("mesero1@gmail.com","mesero1");
@@ -55,17 +53,22 @@ public class Programa extends Application {
     }
 
     @Override
-    public void stop(){
-        System.out.println("fin");
+    public void stop ( )
+    {
+        System.out.println ( "Fin de la Ejecución" );
     }
+    
     @Override
-    public void start(Stage primaryStage)  {
-       VistaPrograma p = new VistaPrograma();
-        sc = new Scene(p.getRoot(),675,600);
-       primaryStage.setScene(sc);
-       primaryStage.show();
+    public void start ( Stage primaryStage )
+    {
+        VistaPrograma vistaPrograma = new VistaPrograma();
+        sc = new Scene ( vistaPrograma.getRoot ( ), 675, 700 );
+        primaryStage.setScene ( sc );
+        primaryStage.show ( );
     }
-       public static void main(String[] args) {
-            launch(args);
+    
+    public static void main ( String [ ] args )
+    {
+        launch ( args );
     }
 }
--- proyectoRest/src/Interfaz/VistaPrograma.java
+++ proyectoRest/src/Interfaz/VistaPrograma.java
@@ -1,110 +1,142 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Interfaz;
 
 import Actores.Administrador;
 import Actores.Mesero;
 import Actores.Persona;
 import static Interfaz.Programa.restaurante;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 /**
  *
  * @author Eddy Santibañez J
  */
-public class VistaPrograma {
-    private  VBox root; 
-    private HBox _seccion_correo; 
-    private HBox _seccion_contrasena;
-
-    public VBox getRoot() {
-        return root;
-    }
-
-    public VistaPrograma() {
-        crearInicio();
+public class VistaPrograma
+{
+    private StackPane root;
+    
+    public VistaPrograma ( )
+    {
+        crearInicio ( );
     }
     
-    
-    
-    
-   public void crearInicio(){
-       root= new VBox();
-       _seccion_correo= new HBox();
-       _seccion_contrasena= new HBox();
-       
-       Label _iniciar= new Label("Iniciar sesión"); 
-       Label _correo= new Label("Correo");
-       TextField _textcorreo= new TextField();
-       Label _contrasena= new Label("Contrasena");
-       TextField _textcontrasena= new TextField();
-       Button _continuar= new Button("Continuar");
-       
-       _seccion_correo.getChildren().addAll(_correo,_textcorreo);
-       _seccion_contrasena.getChildren().addAll(_contrasena,_textcontrasena);
-       root.getChildren().addAll(_iniciar,_seccion_correo,_seccion_contrasena,_continuar);
-       
-      EventHandler<MouseEvent> ev= new EventHandler<MouseEvent>(){
-           @Override
-           public void handle(MouseEvent ev) {
-               String correo_ingresado= _textcorreo.getText();
-               String contraseña_ingresada= _textcontrasena.getText();
-               Persona p_dentro= buscarPersona(correo_ingresado,contraseña_ingresada);
-               if (p_dentro==null){
-                   root.getChildren().clear();
-                   Label _noValido= new Label("Credenciales Incorrectas. Vuelva a inetntarlo ");
-                   System.out.println("nooo");
-                   //mejorarrr******************************
-                   //indicar que no se existee
-               }
-               if(p_dentro instanceof Administrador ){
-                   root.getChildren().clear();
-                   crearInterfazAdmin();
-               }
-               if(p_dentro instanceof Mesero){
-                   //interfaz del mesero llamar
-                   System.out.println("wiii");
-               }
-               
-        }};
-       _continuar.setOnMouseClicked(ev);
+    public void crearInicio ( )
+    {
+        try
+        {
+            root = new StackPane ( );
+            GridPane grilla = new GridPane ( );
+            grilla.setAlignment ( Pos.CENTER );
+            ImageView fondo = new ImageView ( new Image ( new FileInputStream ( "src/recursos/login1.png" ) ) );
+            //fondo.setOpacity ( 0.25 );
+            ImageView img = new ImageView ( new Image ( new FileInputStream ( "src/recursos/usuario.png" ) ) );
+            Label lbl_login = new Label ( "Iniciar Sesión" );
+            Button btn_ingresar = new Button ( "Ingresar");
+            TextField txt_usuario = new TextField ( );
+            TextField txt_contrasenia = new TextField ( );
+            //btn_ingresar.setDefaultButton ( true );
+            
+            grilla.add (img, 0, 0 );
+            grilla.add ( lbl_login, 0, 1 );
+            grilla.add ( new Label ( "Usuario:" ), 0, 2 );
+            grilla.add ( txt_usuario, 1, 2 );
+            grilla.add ( new Label ( "Contraseña:" ), 0, 3 );
+            grilla.add ( txt_contrasenia, 1, 3 );
+            grilla.add ( btn_ingresar, 0, 4  );
+            grilla.setPadding ( new Insets ( 5 ) );
+            grilla.setHgap ( 5 );
+            grilla.setVgap ( 5 );
+            
+            GridPane.setColumnSpan (img, 2);
+            GridPane.setHalignment (img, HPos.CENTER );
+            GridPane.setColumnSpan ( lbl_login, 2);
+            GridPane.setHalignment ( lbl_login, HPos.CENTER );
+            GridPane.setColumnSpan ( btn_ingresar, 2);
+            GridPane.setHalignment ( btn_ingresar, HPos.CENTER );
 
-  }
+            root.getChildren ( ).addAll ( fondo, grilla );
+
+            EventHandler < MouseEvent > ev = new EventHandler < MouseEvent > ( )
+            {
+                @Override
+                public void handle ( MouseEvent ev )
+                {
+                    String correo_ingresado = txt_usuario.getText ( );
+                    String contrasenia_ingresada = txt_contrasenia.getText ( );
+                    Persona p_dentro = buscarPersona ( correo_ingresado, contrasenia_ingresada );
+
+                    if ( p_dentro == null )
+                    {
+                       root.getChildren().clear();
+                       Label _noValido= new Label("Credenciales Incorrectas. Vuelva a inetntarlo ");
+                       System.out.println("nooo");
+                       //mejorarrr******************************
+                       //indicar que no se existee
+                    }
+
+                    if ( p_dentro instanceof Administrador )
+                    {
+                       root.getChildren().clear();
+                       crearInterfazAdmin();
+                    }
+
+                    if ( p_dentro instanceof Mesero )
+                    {
+                       //interfaz del mesero llamar
+                       System.out.println("wiii");
+                    }    
+                }
+            };
+            btn_ingresar.setOnMouseClicked(ev);
+        }
+        catch ( Exception e )
+        {
+            System.out.println ( "Error" );
+        }
+    }
    
-   public Persona buscarPersona(String correo, String contrasena){
-         for (Persona p:  restaurante.getPersonas() ){
-                   if(p.getContrasena().equals(contrasena)&& p.getCorreo().equals(correo)){
-                       return p;
-               }
-               //verificar que exista la persona gg
+    public Persona buscarPersona ( String correo, String contrasena )
+    {
+        for ( Persona persona: restaurante.getPersonas ( ) )
+        {
+            if ( persona.getContrasena ( ).equals ( contrasena ) && persona.getCorreo ( ).equals ( correo ) )
+            {
+                return persona;
             }
+            //verificar que exista la persona gg
+        }
         return null;
-
-}
+    }
     
     public void crearInterfazAdmin ( )
     {
         try
         {
             InterfazAdministrador Interfaz_admin = new InterfazAdministrador ( );
-            VBox adminInterf= Interfaz_admin.getRootA();
+            VBox adminInterf = Interfaz_admin.getRootA();
             root.getChildren().add(adminInterf); 
         }
         catch ( FileNotFoundException e )
         {
             System.out.println ( "Archivo no encontrado" );
         }
-        
+    }
+    
+    public StackPane getRoot ( )
+    {
+        return root;
     }
 }
